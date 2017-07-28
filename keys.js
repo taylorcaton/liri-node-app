d3b793c0b3914eb1fdda9bc45f37c5aa74fefa4a
Taylor Caton <taylorcaton@gmail.com>
2017-07-28 09:12:37 -0400

    initial files

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'DQ17lRhfVN0mcMXOw22JiiRKW ',
+  consumer_secret: 'KXcIZLVi2pxsCrfUgVVCAzDuxo9itc13sl9TCVGq7QGMI0BEju',
+  access_token_key: '890918224220606464-HIah4E7eGOp6rA10nTc4jLfe4NTAHmL',
+  access_token_secret: 'Hkn8Lhyv7lZCiGKAJcQLYu35HK7RXxt5iG9gzlD0ga3pY',
+}
