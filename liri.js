2d0a60363f34f5fbe1db1c59b89b4f1fe95c2b4b
Taylor Caton <taylorcaton@gmail.com>
2017-07-28 17:54:02 -0400

    Added OMDB function

---
+++ liri.js
@@ -0,0 +1,19 @@
+var arg = process.argv[2];
+var arg2 = process.argv[3];
+
+if(arg === "movie-this"){
+    // Include the request npm package (Don't forget to run "npm install request" in this folder first!)
+    var request = require("request");
+
+    // Then run a request to the OMDB API with the movie specified
+    request("http://www.omdbapi.com/?t="+arg2+"&y=&plot=short&apikey=40e9cece", function(error, response, body) {
+
+    // If the request is successful (i.e. if the response status code is 200)
+        if (!error && response.statusCode === 200) {
+
+        // Parse the body of the site and recover just the imdbRating
+        // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+            console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
+        }
+    });
+}
