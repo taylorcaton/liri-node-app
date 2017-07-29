ba7aa611cdad5e2555ac3c62b8b4411afc10d480
Taylor Caton <taylorcaton@gmail.com>
2017-07-28 22:17:33 -0400

    Added Spotify and Twitter functions

--- keys.js
+++ keys.js
@@ -1,8 +1,13 @@
 console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: 'DQ17lRhfVN0mcMXOw22JiiRKW ',
+  consumer_key: 'DQ17lRhfVN0mcMXOw22JiiRKW',
   consumer_secret: 'KXcIZLVi2pxsCrfUgVVCAzDuxo9itc13sl9TCVGq7QGMI0BEju',
   access_token_key: '890918224220606464-HIah4E7eGOp6rA10nTc4jLfe4NTAHmL',
   access_token_secret: 'Hkn8Lhyv7lZCiGKAJcQLYu35HK7RXxt5iG9gzlD0ga3pY',
+}
+
+exports.spotifyKeys = {
+    client_id: '0a4016ea30c4413d8ca33abc0d744e57',
+    client_secret: 'c8a8b3e8749a4220aabd32035c6a6c74'
 }
--- liri.js
+++ liri.js
@@ -1,19 +1,81 @@
 var arg = process.argv[2];
 var arg2 = process.argv[3];
+var keys = require("./keys.js")
 
-if(arg === "movie-this"){
-    // Include the request npm package (Don't forget to run "npm install request" in this folder first!)
-    var request = require("request");
+switch (arg) {
+    case "movie-this":
+        
+        var request = require("request");
 
-    // Then run a request to the OMDB API with the movie specified
-    request("http://www.omdbapi.com/?t="+arg2+"&y=&plot=short&apikey=40e9cece", function(error, response, body) {
+        // Run a request to the OMDB API with the movie specified
+        request("http://www.omdbapi.com/?t="+arg2+"&y=&plot=short&apikey=40e9cece", function(error, response, body) {
 
-    // If the request is successful (i.e. if the response status code is 200)
-        if (!error && response.statusCode === 200) {
+            // If the request is successful (i.e. if the response status code is 200)
+            if (!error && response.statusCode === 200) {
 
-        // Parse the body of the site and recover just the imdbRating
-        // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-            console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
+                // Parse the body of the site and recover just the imdbRating
+                console.log("Title: \t" + JSON.parse(body).Title);
+                console.log("Year : \t" + JSON.parse(body).Year);
+                console.log("Rated: \t" + JSON.parse(body).Rated)
+                console.log("Genre: \t" + JSON.parse(body).Genre);
+                console.log("Plot : \t" + JSON.parse(body).Plot)
+            }
+        });
+
+        break;
+    
+    case "my-tweets":
+            var Twitter = require("twitter");
+
+            var client = new Twitter({
+                consumer_key: keys.twitterKeys.consumer_key,
+                consumer_secret: keys.twitterKeys.consumer_secret,
+                access_token_key: keys.twitterKeys.access_token_key,
+                access_token_secret: keys.twitterKeys.access_token_secret
+            });
+
+            var params = {screen_name: 'TaylorLiri'};
+            client.get('statuses/user_timeline', params, function(error, tweets, response) {
+            
+                // console.log(response);
+                
+                if (!error) {
+                    var myTweets = tweets;
+                    for (var i = 0; i < myTweets.length; i++) {
+                        console.log(myTweets[i].created_at);
+                        console.log(myTweets[i].text + "\n");
+                    }
+                }
+            });
+
+        break;
+    
+    case "spotify-this-song":
+        
+        var Spotify = require('node-spotify-api');
+ 
+        var spotify = new Spotify({
+            id: keys.spotifyKeys.client_id,
+            secret: keys.spotifyKeys.client_secret
+        });
+        
+        spotify.search({ type: 'track', query: arg2, limit: 1 }, function(err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
         }
-    });
+        
+        var songData = data.tracks.items[0];
+        console.log("Artist: \t" + songData.artists[0].name);
+        console.log("Track: \t\t"+ songData.name);
+        console.log("Album: \t\t" + songData.album.name)
+        console.log("Link: \t\t" + songData.external_urls.spotify);
+
+        });
+                
+
+    break;
+
+    
+    default:
+    break;
 }
